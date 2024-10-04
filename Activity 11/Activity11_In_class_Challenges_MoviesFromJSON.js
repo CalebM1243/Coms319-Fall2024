8836610f1f2e72d2f3f6c9078f90575ade5cf6b1
CalebM1243 <calebmoe123@gmail.com>
2024-10-04 17:48:06 -0500

    finished assignment 11

--- Activity 11/Activity11_In_class_Challenges_MoviesFromJSON.js
+++ Activity 11/Activity11_In_class_Challenges_MoviesFromJSON.js
@@ -1,13 +1,33 @@
-function loadMovies(myMovies){
-    const m = document.getElementById("selectedMovie");
-    const inputMovieName = m.value;
+function loadMovies(myMovies,n){
+    const arrayMovies = Object.values(myMovies);
+
+    let sortedMovies = [];
+    switch (n){
+        case 1:
+            sortedMovies = arrayMovies.sort((p1,p2) => (p1.price>p2.price) ? 1 : (p1.price<p2.price) ? -1 : 0);
+            break;
+        case 2:
+            sortedMovies = arrayMovies.sort((p1,p2) => (p1.price>p2.price) ? -1 : (p1.price<p2.price) ? 1 : 0);
+            break;
+        case 3:
+            const inputDescription = document.getElementById("descriptionInput").value;
+            document.getElementById('inputField').style.display = 'none';
+            arrayMovies.forEach(element => {
+               if(element.description.includes(inputDescription)){
+                sortedMovies.push(element);
+               }    
+            });
+            break;
+    }
     var CardMovie = document.getElementById("col");
-    console.log("Selected movie:", inputMovieName);
-    for (let i = 0; i < myMovies.length; i++) {
-        if (myMovies[i].title == inputMovieName) {
-            let title = myMovies[i].title;
-            let year = myMovies[i].year;
-            let url = myMovies[i].url;
+    CardMovie.innerHTML = "";
+    console.log(sortedMovies)
+    console.log(typeof(sortedMovies))
+    for (let i = 0; i < sortedMovies.length; i++) {
+            let title = sortedMovies[i].title;
+            let year = sortedMovies[i].year;
+            let url = sortedMovies[i].url;
+            let price = sortedMovies[i].price;
             console.log(title);
             // construct the HTML element
             let AddCardMovie = document.createElement("div");
@@ -16,32 +36,48 @@ function loadMovies(myMovies){
             <div class="card shadow-sm">
             <img src=${url} class="card-img-top" alt="..."></img>
             <div class="card-body">
-            <p class="card-text"> <strong>${title}</strong>, ${year}</p>
+            <p class="card-text"> <strong>${title}</strong>, ${year}, $${price}</p>
             </div>
             </div>
             `;
+            AddCardMovie.addEventListener('click', function() {
+                if(this.querySelector(".card").style.backgroundColor == "white"){
+                    this.querySelector(".card").style.backgroundColor = 'black'; // Change the background color
+                    this.querySelector(".card-text").style.color = 'white'; // Change text color to ensure readability
+                }
+                else{
+                    this.querySelector(".card").style.backgroundColor = 'white'; // Change the background color
+                    this.querySelector(".card-text").style.color = 'black'; // Change text color to ensure readability
+                }
+            });
             
-            CardMovie.innerHTML = "";
             CardMovie.appendChild(AddCardMovie);
-        } // end of if
     } 
 }
-function idk(){
-    const b = document.getElementById("my_form");
-    
-    b.addEventListener("submit",(event)=>{
-        event.preventDefault(); // Prevent the form from submitting in the traditional way
-        fetch("./MoviesFromJSON.json").then(function (response){
-            if (!response.ok) {
-                throw new Error(`HTTP error! status: ${response.status}`);
-            }
-            return response.json();
-        })
-        .then(function (data){
-            loadMovies(data.movies);
-        })
+function fetchMovies(){
+    return fetch("./MoviesFromJSON.json")
+    .then(response => response.json())
+    .then(json =>{
+        return json;
     });
 }
-document.addEventListener("DOMContentLoaded", function() {
-    idk();
-});
+function showCardsSortedByPriceLowHigh(){
+    fetchMovies()
+    .then(myMovies => loadMovies(myMovies.movies, 1))
+    .catch(err => console.log("Error :"+err));
+}
+function showCardsSortedByPriceHighLow(){
+    fetchMovies()
+    .then(myMovies => loadMovies(myMovies.movies, 2))
+    .catch(err => console.log("Error :"+err));
+}
+function showCardsContainingDescriptionA(){
+    const inputField = document.getElementById('inputField');
+    inputField.style.display = 'block';
+}
+function showCardsContainingDescriptionB(){
+    fetchMovies()
+    .then(myMovies => loadMovies(myMovies.movies, 3))
+    .catch(err => console.log("Error :"+err));
+}
+
